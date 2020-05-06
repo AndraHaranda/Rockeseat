0428ad61a572a9ffc957cf64598353aecd78d008
CabecaDeCenoura <40477620+CabecaDeCenoura@users.noreply.github.com>
2020-05-06 16:39:20 -0300

    JavaScript

---
+++ main.js
@@ -0,0 +1,45 @@
+// AJAX
+// var xhr = new XMLHttpRequest();
+
+// xhr.open('GET', 'https://api.github.com/users/CabecaDeCenoura')
+// xhr.send(null);
+
+// xhr.onreadystatechange = function(){
+//     if (xhr.readyState === 4){
+//         console.log(JSON.parse(xhr.responseText));
+//     }
+// }
+
+var minhaPromise = function(){
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/CabecaDeCenoura');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }
+                else {
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    });
+}
+
+// var resultado = minhaPromise();
+// console.log(resultado);
+
+minhaPromise()
+ .then(function(response) {
+    console.log(response);
+ })
+ 
+ .catch(function(erro){
+    console.warn(erro);
+ });
+
+
+ 
